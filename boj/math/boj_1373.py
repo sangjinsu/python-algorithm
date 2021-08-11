2affe4adbda00273a632f7264c386e074518b3ea
jinsuSang <climbplant39@kakao.com>
2021-08-12 00:00:11 +0900

    update

--- boj/math/boj_1373.py
+++ boj/math/boj_1373.py
@@ -1,9 +1,22 @@
 import sys
 
+
+def binary_to_octal(binary):
+    result = 0
+    k = 1
+    for i in range(len(binary) - 1, -1, -1):
+        result += int(binary[i]) * k
+        k *= 2
+    return str(result)
+
+
 binary = sys.stdin.readline().strip()
 
+result = ''
 for i in range(len(binary), 0, -3):
     if i < 3:
-        print(binary[:i])
+        result = binary_to_octal(binary[:i]) + result
     else:
-        print(binary[i - 3: i])
+        result = binary_to_octal(binary[i - 3: i]) + result
+
+sys.stdout.write(result)
