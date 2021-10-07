94af5550c4786b04f641dde183add36b0e2b5830
jinsuSang <climbplant39@kakao.com>
2021-10-07 16:05:59 +0900

    update

--- swexpert/d3/sw_5208.py
+++ swexpert/d3/sw_5208.py
@@ -4,6 +4,30 @@ import math
 test_cases = int(input().strip())
 
 
+def recursion(idx, remain, swap):
+    global cnt
+
+    if swap >= cnt:
+        return
+    if remain == 0:
+        return
+    if idx >= N - 2:
+        cnt = min(cnt, swap)
+        return
+
+    recursion(idx + 1, station[idx + 1], swap + 1)
+    recursion(idx + 1, remain - 1, swap)
+
+
+for t in range(1, test_cases + 1):
+    nums = list(map(int, input().strip().split()))
+    N = nums[0]
+    station = nums[1:]
+    cnt = math.inf
+    recursion(0, station[0], 0)
+    print('#{} {}'.format(t, cnt))
+
+'''
 def recursion(idx, swap):
     global cnt
 
@@ -25,3 +49,4 @@ for t in range(1, test_cases + 1):
     cnt = math.inf
     recursion(0, -1)
     print('#{} {}'.format(t, cnt))
+'''
