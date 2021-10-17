e316c551085cb5f029efc42b18a4ca6e685b88a6
jinsuSang <climbplant39@kakao.com>
2021-10-18 00:22:58 +0900

    binary counting

--- ssafy/bit/binary_counting.py
+++ ssafy/bit/binary_counting.py
@@ -6,3 +6,25 @@ for i in range(1, 1 << n):
         if i & (1 << j):
             print(arr[j], end=' ')
     print()
+
+print()
+print()
+
+nums = [1, 2, 3, 4, 5]
+visited = [0, 0, 0, 0, 0]
+ans_list = []
+
+
+def nCr(n, r, cnt):
+    if n == len(nums):
+        if cnt == r:
+            ans_list.append([nums[i] for i in range(5) if visited[i]])
+        return
+    visited[n] = 1
+    nCr(n + 1, r, cnt + 1)
+    visited[n] = 0
+    nCr(n + 1, r, cnt)
+
+
+nCr(0, 3, 0)
+print(ans_list)
