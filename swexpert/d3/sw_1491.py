537164d1967ff27e291d3e0c8dc91b7071f84030
jinsuSang <climbplant39@kakao.com>
2021-10-22 08:49:01 +0900

    update

--- swexpert/d3/sw_1491.py
+++ swexpert/d3/sw_1491.py
@@ -1,12 +1,9 @@
-import math
-
-INF = math.inf
 test_cases = int(input().strip())
 for t in range(1, test_cases + 1):
     N, A, B = map(int, input().strip().split())
 
-    result = INF
-    for R in range(1, N // 2 + 1):
-        for C in range(1, N // R + 1):
+    result = 10 ** 9
+    for R in range(1, int(N ** 0.5) + 1):
+        for C in range(R, N // R + 1):
             result = min(result, A * abs(R - C) + B * (N - R * C))
     print('#{} {}'.format(t, result))
--- swexpert/d3/sw_3304.py
+++ swexpert/d3/sw_3304.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 test_cases = int(input().strip())
 
 for t in range(1, test_cases + 1):
@@ -13,3 +15,16 @@ for t in range(1, test_cases + 1):
             else:
                 dp[j][i] = max(dp[j - 1][i], dp[j][i - 1])
     print('#{} {}'.format(t, dp[len(B) - 1][len(A) - 1]))
+    pprint(dp)
+"""
+    '' a  c  a  y  k  p
+''[[0, 0, 0, 0, 0, 0, 0],
+c  [0, 0, 1, 1, 1, 1, 1],
+a  [0, 1, 1, 2, 2, 2, 2],
+p  [0, 1, 1, 2, 2, 2, 3],
+c  [0, 1, 2, 2, 2, 2, 3],
+a  [0, 1, 2, 3, 3, 3, 3],
+k  [0, 1, 2, 3, 3, 4, 4]]
+a    ''
+cap  capa
+"""
