ddcd821e6ea3e3ce4e43faca9e0b64523d99c50e
sangjinsu <climbplant39@kakao.com>
2022-08-21 17:51:26 +0900

    10809

--- boj/implementation/boj_10809.py
+++ boj/implementation/boj_10809.py
@@ -3,9 +3,11 @@ import sys
 word = sys.stdin.readline().strip()
 
 result = [-1] * (ord('z') - ord('a') + 1)
+idx_chars = [(i, word[i]) for i in range(len(word))]
 
-for i, letter in enumerate(word):
-    if result[ord(letter) - ord('a')] == -1:
-        result[ord(letter) - ord('a')] = i
+for idx_char in idx_chars:
+    idx = ord(idx_char[1]) - ord('a')
+    if result[idx] < 0:
+        result[idx] = idx_char[0]
 
 sys.stdout.write(' '.join(map(str, result)))
